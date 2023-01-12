8f2c8914bab19ea6a4dc35ef4ac9921e5372273a
bropel <bryceopel@gmail.com>
2023-01-11 20:09:18 -0500

    Updating and adding files

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 
+const jsonReadFromFile = fs.readFileSync('./grocerylist.json','utf-8');
+const savedList = JSON.parse(jsonReadFromFile);
+console.log(savedList);
+
 const prompt = require('prompt');
 
 prompt.start();
 
-generateGroceryList = function() {
+let groceryList;
 
-    class GroceryItem {
-        constructor (item, quantity, price){
-            this.item = item;
-            this.quantity = quantity;
-            this.price = price;
-        }
+class GroceryItem {
+    constructor (item, quantity, price){
+        this.item = item;
+        this.quantity = quantity;
+        this.price = price;
     }
+}
+
+generateGroceryList = function() {
 
 prompt.get(['item','quantity','price'], function (err, result) {
     if (err) { return onErr(err); }
@@ -23,11 +29,12 @@ prompt.get(['item','quantity','price'], function (err, result) {
 
     const item1 = new GroceryItem(result.item,result.quantity,result.price);
 
-    let groceryList = new Array();
+    groceryList = new Array();
 
 
     groceryList.push(item1);
     console.log(groceryList);
+    return groceryList;
 
 
 });
@@ -38,3 +45,27 @@ function onErr(err) {
 }
 
 }
+
+//console.log(groceryList);
+let list = groceryList;
+
+saveProcess = function(){
+    prompt.get(['Do you want to save your list? (y/n)'], function (err,result){
+        let temp = result;
+        if (err) { return onErr(err); }
+        if (temp == 'y'){
+            let jsonString = JSON.stringify(list);
+            fs.writeFileSync('./grocerylist.json', jsonString);
+            
+        } else {
+            generateGroceryList();
+        }
+
+    });
+}
+
+
+generateGroceryList();
+//saveProcess();
+
+//prompt('Do you want to save and exit?')
